3afc50981c363e20cdb578ab325eaefbb7f82a9c
mgechev <mgechev@gmail.com>
2016-02-11 16:24:54 +0200

    Add tests

--- test/inputParameterDecoratorRule.spec.ts
+++ test/inputParameterDecoratorRule.spec.ts
@@ -1,7 +1,7 @@
-import {assertFailure} from './testHelper';
+import {assertFailure, assertSuccess} from './testHelper';
 
 describe('component-selector', () => {
-  it('should fail when "inputs" is used', () => {
+  it('should fail when "inputs" is used in @Component', () => {
     let source = `
       @Component({
         inputs: [
@@ -12,6 +12,36 @@ describe('component-selector', () => {
     `;
     assertFailure('input-parameter-decorator', source, {
       message: 'In the "@Component" class decorator of the class "Bar" you are using the "inputs" property, this is considered bad practice. Use "@Input" property decorator instead.',
+      startPosition: {
+        line: 2,
+        character: 8
+      },
+      endPosition: {
+        line: 4,
+        character: 9
+      }
+    });
+  });
+  it('should succeed when "inputs" is not used', () => {
+    let source = `
+      @Component({
+        selector: 'baz'
+      })
+      class Bar {}
+    `;
+    assertSuccess('input-parameter-decorator', source);
+  });
+  it('should fail when "inputs" is used in @Directive', () => {
+    let source = `
+      @Directive({
+        inputs: [
+          'id: foo'
+        ]
+      })
+      class Baz {}
+    `;
+    assertFailure('input-parameter-decorator', source, {
+      message: 'In the "@Directive" class decorator of the class "Baz" you are using the "inputs" property, this is considered bad practice. Use "@Input" property decorator instead.',
       startPosition: {
         line: 2,
         character: 8
