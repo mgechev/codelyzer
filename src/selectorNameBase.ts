4cd830b7a7e084248ae0e5ebbe43dc9c57860690
mgechev <mgechev@gmail.com>
2016-02-18 16:05:27 +0200

    Fix issue in lack of decorators

--- src/selectorNameBase.ts
+++ src/selectorNameBase.ts
@@ -49,7 +49,7 @@ class SelectorNameValidatorWalker extends Lint.RuleWalker {
   }
 
   visitClassDeclaration(node: ts.ClassDeclaration) {
-    node.decorators.forEach(this.validateDecorator.bind(this, node.name.text));
+    (node.decorators || []).forEach(this.validateDecorator.bind(this, node.name.text));
     super.visitClassDeclaration(node);
   }
 
