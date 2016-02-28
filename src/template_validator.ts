e18ba69fb4834678e110d55c5c980708825231df
mgechev <mgechev@gmail.com>
2016-02-28 17:17:46 +0200

    feat: add component template validation

--- src/template_validator.ts
+++ src/template_validator.ts
@@ -9,6 +9,7 @@ import {
   COMMON_DIRECTIVES,
   COMMON_PIPES
 } from 'angular2/common';
+import {CompileDirectiveMetadata, CompilePipeMetadata, CompileTypeMetadata} from 'angular2/src/compiler/directive_metadata';
 import {TemplateParser} from 'angular2/src/compiler/template_parser';
 import {COMPILER_PROVIDERS} from 'angular2/src/compiler/compiler';
 import {Parse5DomAdapter} from 'angular2/src/platform/server/parse5_adapter'
@@ -26,21 +27,42 @@ export class TemplateValidator {
   }
   validate(rootFile: string) {
     let collector = new ComponentMetadataCollector();
-    let tree = collector.getComponentTree(rootFile);
-    tree.forEach(c => this._validateComponent(c));
+    let tree = collector.getComponentTree(rootFile) || [];
+    return tree.map(c => this._validateDirectivesInTemplate(c)).filter(e => !!e).concat(
+           tree.map(c => this._validateExpressionsInTemplate(c)).filter(e => !!e));
   }
-  private _validateComponent(component: ComponentInfo, directives = COMMON_DIRECTIVES, pipes = COMMON_PIPES) {
+  private _validateDirectivesInTemplate(component: ComponentInfo, directives = COMMON_DIRECTIVES, pipes = COMMON_PIPES) {
     let meta = component.metadata;
     if (meta instanceof ComponentMetadata) {
-      directives = directives.concat(meta.directives.map(d => d.metadata));
-      pipes = pipes.concat(meta.pipes.map(p => p.metadata));
+      directives = directives.concat((meta.directives || []).map(this._getDirective.bind(this)));
+      pipes = pipes.concat((meta.pipes || []).map(this._getPipe.bind(this)));
       try {
         this.parser.parse(meta.template, directives, pipes, '');
       } catch (e) {
-        console.log(e);
+        return e;
       }
+      return (meta.directives || []).map(c => this._validateComponent(c, directives, pipes));
     } else {
-      return true;
+      return null;
     }
+  }
+  private _getDirective(dir) {
+    let m = dir.metadata;
+    return CompileDirectiveMetadata.create({
+      selector: m.selector,
+      type: new CompileTypeMetadata({ name: dir.classDeclaration.name.text }),
+      inputs: m.inputs,
+      outputs: m.outputs
+    });
+  }
+  private _getPipe(pipe) {
+    let m = pipe.metadata;
+    return new CompilePipeMetadata({
+      name: m.name,
+      type: new CompileTypeMetadata({ name: pipe.classDeclaration.name.text })
+    });
+  }
+  _validateExpressionsInTemplate() {
+    return [];
   }
 }
--- test/template_validator.spec.ts
+++ test/template_validator.spec.ts
@@ -1,8 +1,4 @@
-// import {PipeInfo} from '../src/walkers/collect_pipe_metadata_walker';
-// import {DirectiveInfo} from '../src/walkers/collect_component_metadata_walker';
-// import {ComponentMetadataCollector} from '../src/component_metadata_collector';
 import {TemplateValidator} from '../src/template_validator';
-// import {ComponentMetadata} from 'angular2/core';
 import * as chai from 'chai';
 import {join, normalize} from 'path';
 
@@ -10,6 +6,10 @@ describe('template_validator', () => {
   it('should validate template', () => {
     let cmpPath = normalize(join(__dirname, '..', '..', 'sample_data', 'cmp_a.ts'));
     let validator = new TemplateValidator();
-    validator.validate(cmpPath);
+    let errors = validator.validate(cmpPath);
+    chai.assert.equal(errors[0].message,
+`Template parse errors:
+The pipe 'p2' could not be found ("<b [foo]='bar'></b>[ERROR ->]
+{{'foo' | p2}}"): @0:19`);
   });
 });
