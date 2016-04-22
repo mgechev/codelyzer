d4fba289afb471dbfe8e010140363ea3d5e660f8
PreskoIsTheGreatest <preskosemov@abv.bg>
2016-04-22 16:11:06 +0300

    add component/directive rule with update README

---
+++ src/componentClassSuffixRule.ts
@@ -0,0 +1,42 @@
+import * as Lint from 'tslint/lib/lint';
+import * as ts from 'typescript';
+import {sprintf} from 'sprintf-js';
+import SyntaxKind = require('./util/syntaxKind');
+
+export class Rule extends Lint.Rules.AbstractRule {
+
+    public apply(sourceFile:ts.SourceFile):Lint.RuleFailure[] {
+        return this.applyWithWalker(
+            new ClassMetadataWalker(sourceFile,
+                this.getOptions()));
+    }
+
+    static FAILURE:string = "The name of the class %s should end with the suffix Component";
+
+    static validate(className:string):boolean {
+        return /.*Component$/.test(className);
+    }
+}
+
+export class ClassMetadataWalker extends Lint.RuleWalker {
+
+    visitClassDeclaration(node:ts.ClassDeclaration) {
+        let decorators = node.decorators;
+        if (decorators) {
+            let components:Array<string> = decorators.map(d=>(<any>d.expression).expression.text).filter(t=>t === 'Component');
+            if (components.length !== 0) {
+                let name = node.name;
+                let className:string = name.text;
+                if (!Rule.validate(className)) {
+                    this.addFailure(
+                        this.createFailure(
+                            name.getStart(),
+                            name.getWidth(),
+                            sprintf.apply(this, [Rule.FAILURE, className])));
+                }
+            }
+        }
+        super.visitClassDeclaration(node);
+    }
+
+}
