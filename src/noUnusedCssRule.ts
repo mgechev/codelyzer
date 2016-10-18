1b0116933eb2a03d3659d4b692039443e30773da
mgechev <mgechev@gmail.com>
2016-10-18 14:33:51 -0700

    fix(noUnusedCss): usability and perf improvements

--- src/noUnusedCssRule.ts
+++ src/noUnusedCssRule.ts
@@ -6,11 +6,15 @@ import {BasicCssAstVisitor} from './angular/styles/basicCssAstVisitor';
 import {BasicTemplateAstVisitor} from './angular/templates/basicTemplateAstVisitor';
 import {
   TemplateAst,
-  ElementAst
+  ElementAst,
+  PropertyBindingType
 } from '@angular/compiler';
 import {parseTemplate} from './angular/templates/templateParser';
-import {CssAst, CssSelectorAst} from './angular/styles/cssAst';
+import {CssAst, CssSelectorRuleAst, CssSelectorAst} from './angular/styles/cssAst';
 
+const CssSelectorTokenizer = require('css-selector-tokenizer');
+
+// Initialize the selector accessors
 const lang = require('cssauron')({
   tag(node: ElementAst) {
     return (node.name || '').toLowerCase();
@@ -18,11 +22,18 @@ const lang = require('cssauron')({
   // We do not support it for now
   contents(node: ElementAst) { return ''; },
   id(node: ElementAst) {
-    return node.attrs.filter(a => a.name.toLowerCase() === 'id').pop().value;
+    return this.attr(node, 'id');
   },
-  // Will get more complicated with ngClass and [class.foo]
   'class'(node: ElementAst) {
-    return node.attrs.filter(a => a.name.toLowerCase() === 'class').pop().value;
+    const classBindings = (node.inputs || [])
+      .filter(b => b.type === PropertyBindingType.Class)
+      .map(b => b.name).join(' ');
+    const classAttr = node.attrs.filter(a => a.name.toLowerCase() === 'class').pop();
+    let staticClasses = '';
+    if (classAttr) {
+      staticClasses = classAttr.value + ' ';
+    }
+    return staticClasses + classBindings;
   },
   parent(node: any) {
     return node.parentNode;
@@ -31,10 +42,15 @@ const lang = require('cssauron')({
     return node.children;
   },
   attr(node: ElementAst, attr: string) {
-    return node.attrs.filter(a => a.name === attr).pop().value;
+    const targetAttr = node.attrs.filter(a => a.name === attr).pop();
+    if (targetAttr) {
+      return targetAttr.value;
+    }
+    return undefined;
   }
 });
 
+// Visitor which normalizes the elements and finds out if we have a match
 class ElementVisitor extends BasicTemplateAstVisitor {
   visitElement(ast: ElementAst, fn: any) {
     fn(ast);
@@ -44,6 +60,44 @@ class ElementVisitor extends BasicTemplateAstVisitor {
       }
       this.visit(c, fn);
     });
+  }
+}
+
+// Finds out if selector of given type has been used
+const hasSelector = (s: any, type: string) => {
+  if (!s) {
+    return false;
+  }
+  if (s.type === 'selector' || s.type === 'selectors') {
+    return (s.nodes || []).some(n => hasSelector(n, type));
+  } else {
+    return s.type === type;
+  }
+};
+
+const dynamicFilters = {
+  id(ast: ElementAst, selector: any) {
+    return (ast.inputs || []).some(i => i.name === 'id');
+  },
+  attribute(ast: ElementAst, selector: any) {
+    return (ast.inputs || []).some(i => i.type === PropertyBindingType.Attribute);
+  },
+  'class'(ast: ElementAst, selector: any) {
+    return (ast.inputs || []).some(i => i.name === 'className' || i.name === 'ngClass');
+  }
+};
+
+// Filters elements following the strategies:
+// - If has selector by id and any of the elements has a dynamically set id we just skip it.
+// - If has selector by class and any of the elements has a dynamically set class we just skip it.
+// - If has selector by attribute and any of the elements has a dynamically set attribute we just skip it.
+class ElementFilterVisitor extends BasicTemplateAstVisitor {
+  shouldVisit(ast: ElementAst, strategies: any, selectorTypes: any): boolean {
+    return Object.keys(strategies).every(s => {
+      const strategy = strategies[s];
+      return !selectorTypes[s] || !strategy(ast);
+    }) && (ast.children || [])
+      .every(c => ast instanceof ElementAst && this.shouldVisit(<ElementAst>c, strategies, selectorTypes));
   }
 }
 
@@ -61,16 +115,42 @@ export class Rule extends Lint.Rules.AbstractRule {
 
 class UnusedCssVisitor extends BasicCssAstVisitor {
   templateAst: TemplateAst;
+
+  visitCssSelectorRule(ast: CssSelectorRuleAst) {
+    const match = ast.selectors.some(s => this.visitCssSelector(s));
+    if (!match) {
+      this.addFailure(this.createFailure(this.templateStart + ast.start.offset,
+        ast.end.offset - ast.start.offset, 'Unused styles'));
+    }
+  }
+
   visitCssSelector(ast: CssSelectorAst) {
     const parts = [];
     for (let i = 0; i < ast.selectorParts.length; i += 1) {
       const c = ast.selectorParts[i];
-      if (c.strValue.indexOf('::') >= 0) {
+      c.strValue = c.strValue.split('::').shift();
+      // Stop on /deep/ and >>>
+      if (c.strValue.endsWith('/') ||
+          c.strValue.endsWith('>')) {
+        parts.push(c.strValue);
         break;
+      } else if (!c.strValue.startsWith(':')) { // skip :host
+        parts.push(c.strValue);
       }
-      parts.push(c.strValue);
+    }
+    if (!parts.length) {
+      return true;
     }
     const strippedSelector = parts.map(s => s.replace(/\/|>$/, '').trim()).join(' ');
+    const elementFilterVisitor = new ElementFilterVisitor(this.getSourceFile(), this._originalOptions, this.context, 0);
+    const tokenized = CssSelectorTokenizer.parse(strippedSelector);
+    const selectorTypesCache = Object.keys(dynamicFilters).reduce((a: any, key: string) => {
+      a[key] = hasSelector(tokenized, key);
+      return a;
+    }, {});
+    if (!elementFilterVisitor.shouldVisit(<ElementAst>this.templateAst, dynamicFilters, selectorTypesCache)) {
+      return true;
+    }
     let matchFound = false;
     const selector = (element: ElementAst) => {
       if (lang(strippedSelector)(element)) {
@@ -81,13 +161,11 @@ class UnusedCssVisitor extends BasicCssAstVisitor {
     };
     const visitor = new ElementVisitor(this.getSourceFile(), this._originalOptions, this.context, 0);
     visitor.visit(this.templateAst, selector);
-    if (!matchFound) {
-      this.addFailure(this.createFailure(this.templateStart + ast.start.offset,
-        strippedSelector.length, 'Unused style'));
-    }
+    return matchFound;
   }
 }
 
+// Finds the template and wrapes the parsed content into a root element
 export class UnusedCssNg2Visitor extends Ng2Walker {
   private templateAst: TemplateAst;
 
---
+++ test/noUnusedCssRule.spec.ts
@@ -0,0 +1,576 @@
+import {assertFailure, assertSuccess} from './testHelper';
+
+describe('no-unused-css', () => {
+  describe('valid cases', () => {
+
+    it('should succeed when having valid simple selector', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: '<div bar="{{baz}}" [ngClass]="expr">{{ foo }}</div>',
+          styles: [
+            \`
+            div {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertSuccess('no-unused-css', source);
+    });
+
+    describe('complex selectors', () => {
+
+      it('should succeed when having valid complex selector', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div>
+              <section>
+                <span><h1>{{ foo }}</h1></span>
+              </section>
+            </div>\`,
+            styles: [
+              \`
+              div h1 {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+          assertSuccess('no-unused-css', source);
+      });
+
+      it('should succeed when having valid complex selector', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div [class.bar]="true"></div>\`,
+            styles: [
+              \`
+              div.bar {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+          assertSuccess('no-unused-css', source);
+      });
+
+      it('should succeed when having valid complex selector', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div>
+              <section>
+                <span><h1 id="header">{{ foo }}</h1></span>
+              </section>
+            </div>\`,
+            styles: [
+              \`
+              div h1#header {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+          assertSuccess('no-unused-css', source);
+      });
+
+      describe('multiple styles', () => {
+
+        it('should succeed when having valid complex selector', () => {
+          let source = `
+            @Component({
+              selector: 'foobar',
+              template: \`<div>
+                <section>
+                  <span><h1 id="header">{{ foo }}</h1></span>
+                </section>
+              </div>\`,
+              styles: [
+                \`
+                div h1#header {
+                  color: red;
+                }
+                \`,
+                \`
+                #header {
+                  font-size: 10px;
+                }
+                \`
+              ]
+            })
+            class Test {
+              bar: number;
+            }`;
+            assertSuccess('no-unused-css', source);
+        });
+
+      });
+
+    });
+
+    describe('class setter', () => {
+      it('should succeed when having valid complex selector', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div>
+              <section>
+                <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+              </section>
+            </div>\`,
+            styles: [
+              \`
+              div h1.header {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+          assertSuccess('no-unused-css', source);
+      });
+    });
+
+    describe('dynamic classes', () => {
+
+      it('should skip components with dynamically set classes', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div>
+              <section class="bar {{baz}}">
+                <span><h1 [attr.id]="invalid">{{ foo }}</h1></span>
+              </section>
+            </div>\`,
+            styles: [
+              \`
+              div h1#header {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+        assertSuccess('no-unused-css', source);
+      });
+
+      it('should skip components with dynamically set classes', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div>
+              <section [ngClass]="{ 'bar': true }">
+                <span><h1 id="{{invalid}}">{{ foo }}</h1></span>
+              </section>
+            </div>\`,
+            styles: [
+              \`
+              div h1#header {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+          assertSuccess('no-unused-css', source);
+      });
+    });
+
+  });
+
+  describe('failures', () => {
+    it('should fail when having a complex selector that doesn\'t match anything', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1>{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 10,
+            character: 0
+          },
+          endPosition: {
+            line: 12,
+            character: 0
+          }
+      });
+    });
+
+    it('should fail with multiple styles', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h2>{{ foo }}</h2></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div h2 {
+              color: red;
+            }
+            \`,
+            \`
+            h1 {
+              color: black;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 15,
+            character: 0
+          },
+          endPosition: {
+            line: 17,
+            character: 0
+          }
+      });
+    });
+
+   it('should fail when dynamic selector of not the proper type is used', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section class="bar">
+              <span><h1 [attr.id]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div section.bar h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 10,
+            character: 0
+          },
+          endPosition: {
+            line: 12,
+            character: 0
+          }
+      });
+    });
+
+    describe('class setter', () => {
+
+      it('should succeed when having valid complex selector', () => {
+        let source = `
+          @Component({
+            selector: 'foobar',
+            template: \`<div>
+              <section>
+                <span><h1 [class.head]="bar">{{ foo }}</h1></span>
+              </section>
+            </div>\`,
+            styles: [
+              \`
+              div h1.header {
+                color: red;
+              }
+              \`
+            ]
+          })
+          class Test {
+            bar: number;
+          }`;
+          assertFailure('no-unused-css', source, {
+            message: 'Unused styles',
+            startPosition: {
+              line: 10,
+              character: 0
+            },
+            endPosition: {
+              line: 12,
+              character: 0
+            }
+        });
+      });
+    });
+  });
+
+  describe('host', () => {
+
+    it('should never fail for :host', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            :host {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertSuccess('no-unused-css', source);
+    });
+
+    it('should fail when not matched selectors after :host', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            :host section h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 10,
+            character: 0
+          },
+          endPosition: {
+            line: 12,
+            character: 0
+          }
+      });
+    });
+  });
+
+  describe('deep and >>>', () => {
+    it('should ignore deep and match only before it', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div section /deep/ h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertSuccess('no-unused-css', source);
+    });
+
+    it('should match before reaching deep', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <content>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </content>
+          </div>\`,
+          styles: [
+            \`
+            div section /deep/ h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+       assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 10,
+            character: 0
+          },
+          endPosition: {
+            line: 12,
+            character: 0
+          }
+      });
+    });
+
+    it('should ignore deep and match only before it', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div section >>> h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertSuccess('no-unused-css', source);
+    });
+
+    it('should match before reaching deep', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <content>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </content>
+          </div>\`,
+          styles: [
+            \`
+            div section >>> h2 {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+       assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 10,
+            character: 0
+          },
+          endPosition: {
+            line: 12,
+            character: 0
+          }
+      });
+    });
+  });
+
+  describe('pseudo', () => {
+
+    it('should ignore before and after', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div section::before {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertSuccess('no-unused-css', source);
+    });
+
+    it('should ignore before and after', () => {
+      let source = `
+        @Component({
+          selector: 'foobar',
+          template: \`<div>
+            <section>
+              <span><h1 [class.header]="bar">{{ foo }}</h1></span>
+            </section>
+          </div>\`,
+          styles: [
+            \`
+            div content::before {
+              color: red;
+            }
+            \`
+          ]
+        })
+        class Test {
+          bar: number;
+        }`;
+        assertFailure('no-unused-css', source, {
+          message: 'Unused styles',
+          startPosition: {
+            line: 10,
+            character: 0
+          },
+          endPosition: {
+            line: 12,
+            character: 0
+          }
+      });
+    });
+  });
+
+});
