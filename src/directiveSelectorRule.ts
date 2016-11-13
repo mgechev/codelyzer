5e3c25924eb237aa1ff032d8c309059acf422bb2
mgechev <mgechev@gmail.com>
2016-11-12 17:33:00 -0800

    fix: docs & linting errors

--- src/directiveSelectorRule.ts
+++ src/directiveSelectorRule.ts
@@ -1,10 +1,10 @@
 import {SelectorRule} from './selectorNameBase';
 
 export class Rule extends SelectorRule {
-    public getTypeFailure():any { return 'The selector of the directive "%s" should be used as %s ($$02-06$$)'};
-    public getNameFailure():any { return 'The selector of the directive "%s" should be named %s ($$02-06$$)'};
-    getSinglePrefixFailure():any { return 'The selector of the directive "%s" should have prefix "%s" ($$02-08$$)'};
-    getManyPrefixFailure():any { return 'The selector of the directive "%s" should have one of the prefixes: %s ($$02-08$$)'};
+    public getTypeFailure():any { return 'The selector of the directive "%s" should be used as %s ($$02-06$$)'; }
+    public getNameFailure():any { return 'The selector of the directive "%s" should be named %s ($$02-06$$)'; }
+    getSinglePrefixFailure():any { return 'The selector of the directive "%s" should have prefix "%s" ($$02-08$$)'; }
+    getManyPrefixFailure():any { return 'The selector of the directive "%s" should have one of the prefixes: %s ($$02-08$$)'; }
 }
 
 
