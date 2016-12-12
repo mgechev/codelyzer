247279d31649325013f5191edb415fb6fa34b893
Gary Yeung <gary.yeung>
2016-12-11 18:17:23 -0800

    Using WalkerFactory to create Ng2Walker

--- src/componentClassSuffixRule.ts
+++ src/componentClassSuffixRule.ts
@@ -1,34 +1,30 @@
 import * as Lint from 'tslint';
 import * as ts from 'typescript';
 import {sprintf} from 'sprintf-js';
-import {Ng2Walker} from './angular/ng2Walker';
 import {ComponentMetadata} from './angular/metadata';
+import {allNg2Component, Failure} from './walkerFactory/walkerFactory';
+import {Maybe} from './util/function';
 
 export class Rule extends Lint.Rules.AbstractRule {
-  static FAILURE: string = 'The name of the class %s should end with the suffix Component ($$02-03$$)';
+    static FAILURE: string = 'The name of the class %s should end with the suffix Component ($$02-03$$)';
 
-  static validate(className:string):boolean {
-    return /.*Component$/.test(className);
-  }
-
-  public apply(sourceFile:ts.SourceFile):Lint.RuleFailure[] {
-    return this.applyWithWalker(
-      new ClassMetadataWalker(sourceFile,
-        this.getOptions()));
-  }
-}
-
-export class ClassMetadataWalker extends Ng2Walker {
-  visitNg2Component(meta: ComponentMetadata) {
-    let name = meta.controller.name;
-    let className: string = name.text;
-    if (!Rule.validate(className)) {
-      this.addFailure(
-        this.createFailure(
-          name.getStart(),
-          name.getWidth(),
-          sprintf.apply(this, [Rule.FAILURE, className])));
+    static validate(className: string): boolean {
+        return /.*Component$/.test(className);
     }
-    super.visitNg2Component(meta);
-  }
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(
+            allNg2Component()
+                .where((meta: ComponentMetadata) => {
+                    let name = meta.controller.name;
+                    let className: string = name.text;
+                    if (!Rule.validate(className)) {
+                        return Maybe.lift(
+                            new Failure(name, sprintf(Rule.FAILURE, className)))
+                    }
+                    return Maybe.nothing;
+                })
+                .build(sourceFile, this.getOptions())
+        );
+    }
 }
---
+++ src/walkerFactory/walkerFactory.ts
@@ -0,0 +1,47 @@
+import * as ts from "typescript";
+import {Ng2Walker} from '../angular/ng2Walker';
+import {IOptions} from 'tslint';
+import {ComponentMetadata} from '../angular/metadata';
+import {F1, Maybe} from '../util/function';
+
+type Walkable = 'Ng2Component';
+
+export function allNg2Component(): WalkerBuilder<'Ng2Component'> {
+    return new Ng2ComponentWalkerBuilder();
+}
+
+export class Failure {
+    constructor(public node: ts.Node, public message: string) {}
+}
+
+interface WalkerBuilder<T extends Walkable> {
+    where: (validate: F1<ComponentMetadata, Maybe<Failure>>) => WalkerBuilder<T>;
+    build: (sourceFile: ts.SourceFile, options: IOptions) => Ng2Walker;
+}
+
+class Ng2ComponentWalkerBuilder implements WalkerBuilder<'Ng2Component'> {
+    private _where: F1<ComponentMetadata, Maybe<Failure>>;
+
+    where(validate: F1<ComponentMetadata, Maybe<Failure>>):Ng2ComponentWalkerBuilder {
+        this._where = validate;
+        return this;
+    }
+
+    build(sourceFile: ts.SourceFile, options: IOptions): Ng2Walker {
+        const self = this;
+        const e = class extends Ng2Walker {
+            visitNg2Component(meta: ComponentMetadata) {
+                self._where(meta).fmap(failure => {
+                    this.addFailure(
+                        this.createFailure(
+                            failure.node.getStart(),
+                            failure.node.getWidth(),
+                            failure.message,
+                        ));
+                });
+                super.visitNg2Component(meta);
+            }
+        }
+        return new e(sourceFile, options);
+    }
+}
