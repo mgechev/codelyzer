373b152a7aa371895e10ae1726aa6f0d7db7146d
Gary Yeung <ghsyeung@gmail.com>
2016-12-29 01:54:38 -0800

    Refactoring readTemplate and readStyle to use Maybe<T>

--- src/angular/metadataReader.ts
+++ src/angular/metadataReader.ts
@@ -8,19 +8,22 @@ import {logger} from '../util/logger';
 
 import {Config} from './config';
 
-import {DirectiveMetadata, ComponentMetadata, CodeWithSourceMap, TemplateMetadata} from './metadata';
-import {Maybe, unwrapFirst, ifTrue,} from '../util/function';
 import {
-    callExpression, withIdentifier, hasProperties,
-    isSimpleTemplateString, getStringInitializerFromProperty, decoratorArgument
+  DirectiveMetadata, ComponentMetadata, CodeWithSourceMap, TemplateMetadata, StylesMetadata,
+  StyleMetadata
+} from './metadata';
+import {Maybe, unwrapFirst, ifTrue, listToMaybe,} from '../util/function';
+import {
+  callExpression, withIdentifier, hasProperties,
+  isSimpleTemplateString, getStringInitializerFromProperty, decoratorArgument
 } from '../util/astQuery';
 import {getTemplate, getInlineStyle} from '../util/ngQuery';
 
 const normalizeTransformed = (t: CodeWithSourceMap) => {
-    if (!t.map) {
-        t.source = t.code;
-    }
-    return t;
+  if (!t.map) {
+    t.source = t.code;
+  }
+  return t;
 };
 
 
@@ -29,127 +32,115 @@ const normalizeTransformed = (t: CodeWithSourceMap) => {
  */
 export class MetadataReader {
 
-    constructor(private _fileResolver: FileResolver, private _urlResolver?: AbstractResolver) {
-        this._urlResolver = this._urlResolver || new UrlResolver(new PathResolver());
-    }
+  constructor(private _fileResolver: FileResolver, private _urlResolver?: AbstractResolver) {
+    this._urlResolver = this._urlResolver || new UrlResolver(new PathResolver());
+  }
 
-    read(d: ts.ClassDeclaration): DirectiveMetadata {
-        let componentMetadata = unwrapFirst(
-            (d.decorators || ([] as ts.Decorator[])).map((dec: ts.Decorator) => {
-                return Maybe.lift(dec).bind(callExpression)
-                    .bind(withIdentifier('Component'))
-                    .fmap(() => this.readComponentMetadata(d, dec));
-            }));
+  read(d: ts.ClassDeclaration): DirectiveMetadata {
+    let componentMetadata = unwrapFirst(
+      (d.decorators || ([] as ts.Decorator[])).map((dec: ts.Decorator) => {
+        return Maybe.lift(dec).bind(callExpression)
+          .bind(withIdentifier('Component'))
+          .fmap(() => this.readComponentMetadata(d, dec));
+      }));
 
-        let directiveMetadata = unwrapFirst(
-            (d.decorators || ([] as ts.Decorator[])).map((dec: ts.Decorator) =>
-                Maybe.lift(dec)
-                    .bind(callExpression)
-                    .bind(withIdentifier('Directive'))
-                    .fmap(() => this.readDirectiveMetadata(d, dec))
-            ));
+    let directiveMetadata = unwrapFirst(
+      (d.decorators || ([] as ts.Decorator[])).map((dec: ts.Decorator) =>
+        Maybe.lift(dec)
+          .bind(callExpression)
+          .bind(withIdentifier('Directive'))
+          .fmap(() => this.readDirectiveMetadata(d, dec))
+      ));
 
-        return directiveMetadata || componentMetadata || undefined;
-    }
+    return directiveMetadata || componentMetadata || undefined;
+  }
 
-    readDirectiveMetadata(d: ts.ClassDeclaration, dec: ts.Decorator): DirectiveMetadata {
+  protected readDirectiveMetadata(d: ts.ClassDeclaration, dec: ts.Decorator): DirectiveMetadata {
+    const selector = this.getDecoratorArgument(dec)
+      .bind(expr => getStringInitializerFromProperty('selector', expr.properties))
+      .fmap(initializer => initializer.text);
 
-        const selector = this.getDecoratorArgument(dec)
-            .bind(expr => getStringInitializerFromProperty('selector', expr.properties))
-            .fmap(initializer => initializer.text);
+    return Object.assign(new DirectiveMetadata(), {
+      controller: d,
+      decorator: dec,
+      selector: selector.unwrap(),
+    });
+  }
 
-        return Object.assign(new DirectiveMetadata(), {
-            controller: d,
-            decorator: dec,
-            selector: selector.unwrap(),
-        });
-    }
+  protected readComponentMetadata(d: ts.ClassDeclaration, dec: ts.Decorator) {
+    const expr = this.getDecoratorArgument(dec);
+    const directiveMetadata = this.readDirectiveMetadata(d, dec);
 
-    readComponentTemplateMetadata(dec: ts.Decorator, external: MetadataUrls): TemplateMetadata {
-        const template_M = getTemplate(dec)
-            .fmap(inlineTemplate => {
-                const transformed = normalizeTransformed(Config.transformTemplate(inlineTemplate.text, null, dec));
-                return {
-                    template: transformed,
-                    url: null,
-                    node: inlineTemplate,
-                };
-            });
+    const external_M = expr.fmap(() => this._urlResolver.resolve(dec));
 
-        if (template_M.isSomething) {
-            return template_M.unwrap();
-        } else {
-            // TODO: Refactoring this requires adding seem to fileResolver
-            if (external.templateUrl) {
-                try {
-                    const template = this._fileResolver.resolve(external.templateUrl);
-                    const transformed = normalizeTransformed(Config.transformTemplate(template, external.templateUrl, dec));
-                    return {
-                        template: transformed,
-                        url: external.templateUrl,
-                        node: null
-                    };
-                } catch (e) {
-                    logger.info('Cannot read the external template ' + external.templateUrl);
-                }
-            }
+    const template_M: Maybe<TemplateMetadata> = external_M.bind(external =>
+      this.readComponentTemplateMetadata(dec, external));
+    const style_M: Maybe<StylesMetadata> = external_M.bind(external =>
+      this.readComponentStylesMetadata(dec, external));
+
+    return Object.assign(new ComponentMetadata(), directiveMetadata, {
+      template: template_M.unwrap(),
+      styles: style_M.unwrap(),
+    });
+  }
+
+  protected getDecoratorArgument(decorator: ts.Decorator): Maybe<ts.ObjectLiteralExpression> {
+    return decoratorArgument(decorator)
+      .bind(ifTrue(hasProperties));
+  }
+
+  protected readComponentTemplateMetadata(dec: ts.Decorator, external: MetadataUrls): Maybe<TemplateMetadata> {
+    // Resolve Inline template
+    return getTemplate(dec)
+      .fmap(inlineTemplate => ({
+          template: normalizeTransformed(Config.transformTemplate(inlineTemplate.text, null, dec)),
+          url: null,
+          node: inlineTemplate,
+        })
+      ).catch(() =>  // If there's no valid inline template, we resolve external template
+        Maybe.lift(external.templateUrl)
+          .bind(url =>
+            this._resolve(url).fmap(template => ({
+              template: normalizeTransformed(Config.transformTemplate(template, url, dec)),
+              url,
+              node: null
+            }))
+          )
+      );
+  }
+
+  protected readComponentStylesMetadata(dec: ts.Decorator, external: MetadataUrls): Maybe<StylesMetadata> {
+    return getInlineStyle(dec).fmap(inlineStyles =>
+      // Resolve Inline styles
+      inlineStyles.elements.map((inlineStyle: ts.Expression) => {
+        if (isSimpleTemplateString(inlineStyle)) {
+          return {
+            style: normalizeTransformed(Config.transformStyle(inlineStyle.text, null, dec)),
+            url: null,
+            node: inlineStyle as ts.Node,
+          };
         }
+      }).filter(v => !!v)
+    ).catch(() =>  // If there's no valid inline styles, we resolve external styles
+      Maybe.lift(external.styleUrls)
+        .fmap(urls => urls.map(url =>  // Resolve each style URL and transform to metadata
+          this._resolve(url).fmap(style => ({
+            style: normalizeTransformed(Config.transformStyle(style, url, dec)),
+            url,
+            node: null,
+          }))
+        ))
+        // merge Maybe<StyleMetadata>[] to Maybe<StyleMetadata[]>
+        .bind(url_Ms => listToMaybe<StyleMetadata>(url_Ms))
+    );
+  }
+
+  private _resolve(url: string): Maybe<string> {
+    try {
+      return Maybe.lift(this._fileResolver.resolve(url));
+    } catch (e) {
+      logger.info('Cannot read file' + url);
+      return Maybe.nothing;
     }
-
-    readComponentStylesMetadata(dec: ts.Decorator, external: MetadataUrls) {
-        let styles: any[];
-        const inlineStyles_M = getInlineStyle(dec)
-            .fmap(inlineStyles => {
-                return inlineStyles.elements.map((inlineStyle: ts.Expression) => {
-                    if (isSimpleTemplateString(inlineStyle)) {
-                        return {
-                            style: normalizeTransformed(Config.transformStyle(inlineStyle.text, null, dec)),
-                            url: null,
-                            node: inlineStyle,
-                        };
-                    }
-                }).filter(v => !!v);
-            });
-
-        if (inlineStyles_M.isSomething) {
-            return inlineStyles_M.unwrap();
-        } else if (external.styleUrls) {
-            // TODO: Refactoring this requires adding seem to fileResolver
-            try {
-                styles = <any>external.styleUrls.map((url: string) => {
-                    const style = this._fileResolver.resolve(url);
-                    const transformed = normalizeTransformed(Config.transformStyle(style, url, dec));
-                    return {
-                        style: transformed, url,
-                        node: null
-                    };
-                });
-            } catch (e) {
-                logger.info('Unable to read external style. ' + e.toString());
-            }
-        }
-        return styles;
-    }
-
-    readComponentMetadata(d: ts.ClassDeclaration, dec: ts.Decorator) {
-        const expr = this.getDecoratorArgument(dec);
-        const directiveMetadata = this.readDirectiveMetadata(d, dec);
-
-        const external_M = expr.fmap(() => this._urlResolver.resolve(dec));
-
-        const template_M = external_M.fmap(
-            (external) => this.readComponentTemplateMetadata(dec, external));
-        const style_M = external_M.fmap(
-            (external) => this.readComponentStylesMetadata(dec, external));
-
-        return Object.assign(new ComponentMetadata(), directiveMetadata, {
-            template: template_M.unwrap(),
-            styles: style_M.unwrap(),
-        });
-    }
-
-    protected getDecoratorArgument(decorator: ts.Decorator): Maybe<ts.ObjectLiteralExpression> {
-        return decoratorArgument(decorator)
-            .bind(ifTrue(hasProperties));
-    }
+  }
 }
--- src/util/function.ts
+++ src/util/function.ts
@@ -1,81 +1,89 @@
 export interface F0<R> {
-    (): R;
+  (): R;
 }
 export interface F1<A0, R> {
-    (a0: A0): R;
+  (a0: A0): R;
 }
 export interface F2<A0, A1, R> {
-    (a0: A0, a1: A1): R;
+  (a0: A0, a1: A1): R;
 }
 export interface F3<A0, A1, A2, R> {
-    (a0: A0, a1: A1, a2: A2): R;
+  (a0: A0, a1: A1, a2: A2): R;
+}
+
+function nullOrUndef(t: any) {
+  return t === null || t === undefined;
 }
 
 export class Maybe<T> {
 
-    static nothing = new Maybe<any>(undefined);
+  static nothing = new Maybe<any>(undefined);
 
-    static lift<T>(t: T) {
-        if (!t) {
-            return Maybe.nothing;
-        }
-        return new Maybe<T>(t);
+  static lift<T>(t: T) {
+    if (nullOrUndef(t)) {
+      return Maybe.nothing;
     }
+    return new Maybe<T>(t);
+  }
 
-    bind<R>(fn: F1<T, Maybe<R>>): Maybe<R> {
-        if (!!this.t) {
-            return fn(this.t);
-        }
-        return Maybe.nothing;
+  static all<T0, T1>(t0: Maybe<T0>, t1: Maybe<T1>): Maybe<[T0, T1]> {
+    return t0.bind(_t0 => t1.fmap(_t1 => [_t0, _t1] as [T0, T1]));
+  }
+
+  bind<R>(fn: F1<T, Maybe<R>>): Maybe<R> {
+    if (!nullOrUndef(this.t)) {
+      return fn(this.t);
     }
+    return Maybe.nothing;
+  }
 
-    fmap<R>(fn: F1<T, R>): Maybe<R> {
-        return this.bind(t => Maybe.lift(fn(t)));
+  fmap<R>(fn: F1<T, R>): Maybe<R> {
+    return this.bind(t => Maybe.lift(fn(t)));
+  }
+
+  get isNothing() {
+    return nullOrUndef(this.t);
+  }
+
+  get isSomething() {
+    return !nullOrUndef(this.t);
+  }
+
+  catch(def: () => Maybe<T>): Maybe<T> {
+    if (this.isNothing) {
+      return def();
     }
+    return this;
+  }
 
-    get isNothing() {
-        return !this.t;
-    }
+  unwrap(): T | undefined {
+    return this.t;
+  };
 
-    get isSomething() {
-        return !!this.t;
-    }
-
-    catch(def: () => Maybe<T>): Maybe<T> {
-        if (this.isNothing) {
-            return def();
-        }
-        return this;
-    }
-
-    unwrap(): T | undefined {
-        return this.t;
-    };
-
-    private constructor(private t: T | undefined) {
-    }
+  private constructor(private t: T | undefined) {
+  }
 }
 
 export function unwrapFirst<T>(ts: Maybe<T>[]): T|undefined {
-    const f = ts.find((t: Maybe<T>) => t.isSomething);
-    if (!!f) {
-        return f.unwrap();
-    }
-    return undefined;
+  const f = ts.find((t: Maybe<T>) => t.isSomething);
+  if (!!f) {
+    return f.unwrap();
+  }
+  return undefined;
 }
 
 export function all<T>(...preds: F1<T, boolean>[]): F1<T, boolean> {
-    return (t: T) => !preds.find(p => !p(t));
+  return (t: T) => !preds.find(p => !p(t));
 }
 export function any<T>(...preds: F1<T, boolean>[]): F1<T, boolean> {
-    return (t: T) => !!preds.find(p => p(t));
+  return (t: T) => !!preds.find(p => p(t));
 }
 
 export function ifTrue<T>(pred: F1<T, boolean>): F1<T, Maybe<T>> {
-    return (t: T) => (pred(t)) ? Maybe.lift(t) : Maybe.nothing;
+  return (t: T) => (pred(t)) ? Maybe.lift(t) : Maybe.nothing;
 }
 
-export function listToMaybe<T>(ms: Maybe<T>[]):Maybe<T[]> {
-    const unWrapped = ms.filter(m => m.isSomething).map(m => m.unwrap());
-    return unWrapped.length !== 0 ? Maybe.lift(unWrapped) : Maybe.nothing;
+export function listToMaybe<T>(ms: Maybe<T>[]): Maybe<T[]> {
+  const unWrapped = ms.filter(m => m.isSomething).map(m => m.unwrap());
+  return unWrapped.length !== 0 ? Maybe.lift(unWrapped) : Maybe.nothing;
 }
