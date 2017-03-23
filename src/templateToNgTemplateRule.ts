93dfb34713a526f3fde86d0302b8ec0e14ece1ed
mgechev <mgechev@gmail.com>
2017-03-22 21:32:03 -0700

    feat: add automatic migration to ng-template

---
+++ src/templateToNgTemplateRule.ts
@@ -0,0 +1,82 @@
+import * as Lint from 'tslint';
+import * as ts from 'typescript';
+import {stringDistance} from './util/utils';
+import {getDeclaredProperties, getDeclaredMethods} from './util/classDeclarationUtils';
+import {Ng2Walker} from './angular/ng2Walker';
+import {RecursiveAngularExpressionVisitor} from './angular/templates/recursiveAngularExpressionVisitor';
+import * as e from '@angular/compiler/src/expression_parser/ast';
+import { EmbeddedTemplateAst, ElementAst } from '@angular/compiler';
+import { BasicTemplateAstVisitor } from './angular/templates/basicTemplateAstVisitor';
+import { Fix } from 'tslint';
+import SyntaxKind = require('./util/syntaxKind');
+
+const ErrorMessage = 'You should use <ng-template/> instead of <template/>';
+const TemplateStart = '<template';
+const TemplateEnd = '</template>';
+const TemplateEndRe = /<\/template>/i;
+
+const set = new Set<EmbeddedTemplateAst>();
+
+class TemplateToNgTemplateVisitor extends BasicTemplateAstVisitor {
+  private _prevClosing: number = 0;
+  private _fix: Fix;
+  private _visitedElements = new Set<EmbeddedTemplateAst>();
+
+  visitEmbeddedTemplate(element: EmbeddedTemplateAst, ctx: any) {
+    if (this._visitedElements.has(element)) {
+      return;
+    } else {
+      this._visitedElements.add(element);
+    }
+
+    const sp = element.sourceSpan;
+    const content = sp.start.file.content;
+    const subtemplate = content.substring(sp.start.offset, sp.end.offset);
+
+    if (subtemplate.startsWith(TemplateStart)) {
+      const replacement = this.createReplacement(sp.start.offset, TemplateStart.length, '<ng-template');
+      if (!this._fix) {
+        this._fix = this.createFix(replacement);
+      } else {
+        this._fix.replacements.push(replacement);
+      }
+      this.addFailure(this.createFailure(sp.start.offset, sp.end.offset - sp.start.offset, ErrorMessage, this._fix));
+    }
+
+    super.visitEmbeddedTemplate(element, ctx);
+
+    const subcontent = content.substring(this._prevClosing, content.length);
+    const matches = TemplateEndRe.exec(subcontent);
+    if (this._fix && matches && typeof matches.index === 'number') {
+      this._fix.replacements.push(this.createReplacement(matches.index + this._prevClosing, TemplateEnd.length, '</ng-template>'));
+      this._prevClosing = matches.index + this._prevClosing + TemplateEnd.length;
+      const rest = content.substring(this._prevClosing, content.length);
+      if (!TemplateEndRe.test(rest)) {
+        this._fix = null;
+        this._prevClosing = 0;
+      }
+    }
+  }
+}
+
+export class Rule extends Lint.Rules.AbstractRule {
+  public static metadata: Lint.IRuleMetadata = {
+    ruleName: 'templates-use-public-rule',
+    type: 'functionality',
+    description: `Ensure that properties and methods accessed from the template are public.`,
+    rationale: `When Angular compiles the templates, it has to access these propertes from outside the class.`,
+    options: null,
+    optionsDescription: `Not configurable.`,
+    typescriptOnly: true,
+  };
+
+  static FAILURE: string = 'The %s "%s" that you\'re trying to access does not exist in the class declaration.';
+
+  public apply(sourceFile:ts.SourceFile): Lint.RuleFailure[] {
+    return this.applyWithWalker(
+        new Ng2Walker(sourceFile,
+            this.getOptions(), {
+              templateVisitorCtrl: TemplateToNgTemplateVisitor
+            }));
+  }
+}
---
+++ test/templateToNgTemplate.spec.ts
@@ -0,0 +1,282 @@
+import { assertSuccess, assertAnnotated, assertFailure } from './testHelper';
+import chai = require('chai');
+
+describe.only('templates-use-public', () => {
+  it('should skip elements with *-prefixed attr', () => {
+    let source = `
+      @Component({
+        selector: 'foobar',
+        template: '<section><div *ngIf="42"></div></section>'
+      })
+      class Test {}`;
+      assertSuccess('template-to-ng-template', source);
+  });
+
+  it('should detect templete elements', () => {
+    let source = `
+      @Component({
+        selector: 'foobar',
+        template: '<section><template [ngForOf]></template></div>'
+                            ~~~~~~~~~~~~~~~~~~~~
+      })
+      class Test {}`;
+      assertAnnotated({
+        ruleName: 'template-to-ng-template',
+        message: 'You should use <ng-template/> instead of <template/>',
+        source
+      });
+  });
+
+  it('should detect templete elements', () => {
+    let source = `
+      @Component({
+        selector: 'foobar',
+        template: '<section><template [ngForOf]><br></template></div>'
+                            ~~~~~~~~~~~~~~~~~~~~
+      })
+      class Test {}`;
+      assertAnnotated({
+        ruleName: 'template-to-ng-template',
+        message: 'You should use <ng-template/> instead of <template/>',
+        source
+      });
+  });
+
+  it('should detect templete elements', () => {
+    let source = `
+      @Component({
+        selector: 'foobar',
+        template: \`<section>
+          <template [ngForOf]><div/>
+          ~~~~~~~~~~~~~~~~~~~~
+          </template></div>\`
+      })
+      class Test {}`;
+      assertAnnotated({
+        ruleName: 'template-to-ng-template',
+        message: 'You should use <ng-template/> instead of <template/>',
+        source
+      });
+  });
+
+  describe('autofixes', () => {
+
+    it('should not autofix template suggar', () => {
+      let source = `
+      @Component({
+        selector: 'foobar',
+        template: '<section><div *ngIf="42"></div></section>'
+      })
+      class Test {}`;
+      assertSuccess('template-to-ng-template', source);
+    });
+
+    it('should fix template with no suggar', () => {
+      let source = `
+      @Component({
+        selector: 'foobar',
+        template: '<div></div><template></template><template></template>'
+      })
+      class Test {}`;
+      const failures = assertFailure('template-to-ng-template', source, {
+        message: 'You should use <ng-template/> instead of <template/>',
+          startPosition: {
+            line: 3,
+            character: 30
+          },
+          endPosition: {
+            line: 3,
+            character: 40
+          }
+        });
+      chai.expect(failures[0].hasFix()).to.eq(true);
+      const fixes = failures[0].getFix();
+      chai.expect(fixes.replacements.length).to.eq(4);
+      const reps = fixes.replacements;
+      chai.expect(reps[0].text).to.eq('<ng-template');
+      chai.expect(reps[1].text).to.eq('</ng-template>');
+      chai.expect(reps[2].text).to.eq('<ng-template');
+      chai.expect(reps[3].text).to.eq('</ng-template>');
+      chai.expect(reps[0].start).to.eq(78);
+      chai.expect(reps[0].end).to.eq(87);
+      chai.expect(reps[1].start).to.eq(88);
+      chai.expect(reps[1].end).to.eq(99);
+      chai.expect(reps[2].start).to.eq(99);
+      chai.expect(reps[2].end).to.eq(108);
+      chai.expect(reps[3].start).to.eq(109);
+      chai.expect(reps[3].end).to.eq(120);
+      const res = fixes.apply(source);
+      const expected = `
+      @Component({
+        selector: 'foobar',
+        template: '<div></div><ng-template></ng-template><ng-template></ng-template>'
+      })
+      class Test {}`;
+      chai.expect(res).to.eq(expected);
+    });
+
+    it('should fix template with no suggar & nested templates', () => {
+      let source = `
+      @Component({
+        selector: 'foobar',
+        template: '<div></div><template><template></template></template>'
+      })
+      class Test {}`;
+      const failures = assertFailure('template-to-ng-template', source, {
+        message: 'You should use <ng-template/> instead of <template/>',
+          startPosition: {
+            line: 3,
+            character: 30
+          },
+          endPosition: {
+            line: 3,
+            character: 40
+          }
+        });
+      chai.expect(failures[0].hasFix()).to.eq(true);
+      const fixes = failures[0].getFix();
+      chai.expect(fixes.replacements.length).to.eq(4);
+      const reps = fixes.replacements;
+      const res = fixes.apply(source);
+      const expected = `
+      @Component({
+        selector: 'foobar',
+        template: '<div></div><ng-template><ng-template></ng-template></ng-template>'
+      })
+      class Test {}`;
+      chai.expect(res).to.eq(expected);
+    });
+
+    it('should fix template with templates & suggar', () => {
+      let source = `
+      @Component({
+        selector: 'foobar',
+        template: \`
+          <div *ngIf="bar"></div>
+          <template>
+          </template>\`
+      })
+      class Test {}`;
+      const failures = assertFailure('template-to-ng-template', source, {
+        message: 'You should use <ng-template/> instead of <template/>',
+          startPosition: {
+            line: 5,
+            character: 10
+          },
+          endPosition: {
+            line: 5,
+            character: 20
+          }
+        });
+      chai.expect(failures[0].hasFix()).to.eq(true);
+      const fixes = failures[0].getFix();
+      chai.expect(fixes.replacements.length).to.eq(2);
+      const reps = fixes.replacements;
+      const res = fixes.apply(source);
+      const expected = `
+      @Component({
+        selector: 'foobar',
+        template: \`
+          <div *ngIf="bar"></div>
+          <ng-template>
+          </ng-template>\`
+      })
+      class Test {}`;
+      chai.expect(res).to.eq(expected);
+    });
+
+    it('should fix template with no suggar & nested templates & nested elements', () => {
+      let source = `
+      @Component({
+        selector: 'foobar',
+        template: \`
+          <template>
+            <template>
+              <div *ngIf="foo"></div>
+            </template>
+            <template>
+              <div><span>
+              </span></div>
+            </template>
+          </template>
+        \`
+      })
+      class Test {}`;
+      const failures = assertFailure('template-to-ng-template', source, {
+        message: 'You should use <ng-template/> instead of <template/>',
+          startPosition: {
+            line: 4,
+            character: 10
+          },
+          endPosition: {
+            line: 4,
+            character: 20
+          }
+        });
+      chai.expect(failures[0].hasFix()).to.eq(true);
+      const fixes = failures[0].getFix();
+      chai.expect(fixes.replacements.length).to.eq(6);
+      const reps = fixes.replacements;
+      const res = fixes.apply(source);
+      const expected = `
+      @Component({
+        selector: 'foobar',
+        template: \`
+          <ng-template>
+            <ng-template>
+              <div *ngIf="foo"></div>
+            </ng-template>
+            <ng-template>
+              <div><span>
+              </span></div>
+            </ng-template>
+          </ng-template>
+        \`
+      })
+      class Test {}`;
+      chai.expect(res).to.eq(expected);
+    });
+
+    it('should fix template with no suggar & nested templates & nested elements', () => {
+      let source = `
+      @Component({
+        selector: 'foobar',
+        template: \`
+          <div *ngIf="foo">
+            <template></template>
+          </div>
+        \`
+      })
+      class Test {}`;
+      const failures = assertFailure('template-to-ng-template', source, {
+        message: 'You should use <ng-template/> instead of <template/>',
+          startPosition: {
+            line: 5,
+            character: 12
+          },
+          endPosition: {
+            line: 5,
+            character: 22
+          }
+        });
+      chai.expect(failures[0].hasFix()).to.eq(true);
+      const fixes = failures[0].getFix();
+      chai.expect(fixes.replacements.length).to.eq(2);
+      const reps = fixes.replacements;
+      const res = fixes.apply(source);
+      const expected = `
+      @Component({
+        selector: 'foobar',
+        template: \`
+          <div *ngIf="foo">
+            <ng-template></ng-template>
+          </div>
+        \`
+      })
+      class Test {}`;
+      chai.expect(res).to.eq(expected);
+    });
+
+  });
+
+});
