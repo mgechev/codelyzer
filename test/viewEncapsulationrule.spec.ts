939967c240893f1eb079cde40fbaec246bd49312
Gregor Woiwode <gw@co-it.eu>
2017-05-03 00:25:04 +0200

    feat(use-view-encapsulation): add test sources beeing linted

--- test/viewEncapsulationrule.spec.ts
+++ test/viewEncapsulationrule.spec.ts
@@ -1,10 +1,36 @@
 describe('use-view-encapsulation', () => {
   describe('invalid view encapsulation', () => {
-    it('should fail if ViewEncapsulation.None is set');
+    it('should fail if ViewEncapsulation.None is set', () => {
+      const source = `
+        @Component({
+          selector: 'sg-foo-bar',
+          encapsulation: ViewEncapsulation.None
+                                           ~~~~
+        })
+        export class TestComponent { }
+      `;
+    });
   });
 
   describe('valid view encapsulation', () => {
-    it('should succeed if ViewEncapsulation.Native is set');
-    it('should succeed if ViewEncapsulation.Emulated is set');
+    it('should succeed if ViewEncapsulation.Native is set', () => {
+      const source = `
+        @Component({
+          selector: 'sg-foo-bar',
+          encapsulation: ViewEncapsulation.Native
+        })
+        export class TestComponent { }
+      `;
+    });
+
+    it('should succeed if ViewEncapsulation.Emulated is set', () => {
+      const source = `
+        @Component({
+          selector: 'sg-foo-bar',
+          encapsulation: ViewEncapsulation.Emulated
+        })
+        export class TestComponent { }
+      `;
+    });
   });
 });
