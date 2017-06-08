f227c7c994a31419cd09412d8ceb0d0cf9a30f47
Connor Peet <connor@peet.io>
2017-06-08 11:34:54 -0700

    feat: add fixer for the negated async pipe rule

--- src/templatesNoNegatedAsyncRule.ts
+++ src/templatesNoNegatedAsyncRule.ts
@@ -5,32 +5,55 @@ import {RecursiveAngularExpressionVisitor} from './angular/templates/recursiveAn
 import * as e from '@angular/compiler/src/expression_parser/ast';
 import * as ast from '@angular/compiler';
 
+const unstrictEqualityOperator = '==';
+
 class TemplateToNgTemplateVisitor extends RecursiveAngularExpressionVisitor {
   visitBinary(expr: e.Binary, context: any): any {
     if (!this.isAsyncBinding(expr.left)) {
       return super.visitBinary(expr, context);
     }
-    if (!(expr.right instanceof ast.LiteralPrimitive) || expr.right.value !== false || expr.operation !== '==') {
+    if (!(expr.right instanceof ast.LiteralPrimitive) || expr.right.value !== false || expr.operation !== unstrictEqualityOperator) {
       return super.visitBinary(expr, context);
     }
+
+    const operator = this.codeWithMap.code.slice(expr.left.span.end, expr.right.span.start);
+    const operatorStart = (/^.*==/).exec(operator)[0].length - unstrictEqualityOperator.length;
 
     this.addFailure(this.createFailure(
       expr.span.start,
       expr.span.end - expr.span.start,
       `Async pipes must use strict equality \`===\` when comparing with \`false\``,
+      [
+        new Lint.Replacement(
+          this.getSourcePosition(expr.left.span.end) + operatorStart,
+          unstrictEqualityOperator.length,
+          '===',
+        ),
+      ]
     ));
   }
 
   visitPrefixNot(expr: e.PrefixNot, context: any): any {
-    if (this.isAsyncBinding(expr.expression)) {
-      this.addFailure(this.createFailure(
-        expr.span.start,
-        expr.span.end - expr.span.start,
-        `Async pipes can not be negated, use (observable | async) === false instead`,
-      ));
-    } else {
-      super.visitPrefixNot(expr, context);
+    if (!this.isAsyncBinding(expr.expression)) {
+      return super.visitPrefixNot(expr, context);
     }
+
+    const width = expr.span.end - expr.span.start;
+    const absoluteStart = this.getSourcePosition(expr.span.start);
+
+    // Angular includes the whitespace after an expression, we want to trim that
+    const expressionSource = this.codeWithMap.code.slice(expr.span.start, expr.span.end);
+    const concreteWidth = width - (/ *$/).exec(expressionSource)[0].length;
+
+    this.addFailure(this.createFailure(
+      expr.span.start,
+      width,
+      `Async pipes can not be negated, use (observable | async) === false instead`,
+      [
+        new Lint.Replacement(absoluteStart + concreteWidth, 1, ' === false '),
+        new Lint.Replacement(absoluteStart, 1, ''),
+      ],
+    ));
   }
 
   protected isAsyncBinding(expr: any) {
--- test/templatesNoNegatedAsyncRule.spec.ts
+++ test/templatesNoNegatedAsyncRule.spec.ts
@@ -1,4 +1,6 @@
 import { assertSuccess, assertAnnotated } from './testHelper';
+import { Replacement } from 'tslint';
+import { expect } from 'chai';
 
 describe('templates-no-negated-async', () => {
   describe('invalid expressions', () => {
@@ -38,19 +40,69 @@ describe('templates-no-negated-async', () => {
 
     it('should fail when an async pipe uses non-strict equality', () => {
       let source = `
+      @Component({
+        selector: 'foobar',
+        template: '{{ (foo | async) == false }}'
+                       ~~~~~~~~~~~~~~~~~~~~~~
+      })
+      class Test {
+        constructor(public foo: Observable<Boolean>) {}
+      }`;
+      assertAnnotated({
+        ruleName: 'templates-no-negated-async',
+        message: 'Async pipes must use strict equality `===` when comparing with `false`',
+        source
+      });
+    });
+
+    describe('fixes', () => {
+      it('fixes negated pipes', () => {
+        let source = `
+        @Component({
+          selector: 'foobar',
+          template: '{{ !(foo | async) }}'
+                        ~~~~~~~~~~~~~~~
+        })
+        class Test {}`;
+        const failures =  assertAnnotated({
+          ruleName: 'templates-no-negated-async',
+          message: 'Async pipes can not be negated, use (observable | async) === false instead',
+          source
+        });
+
+        const res = Replacement.applyAll(source, failures[0].getFix());
+        expect(res).to.eq(`
+        @Component({
+          selector: 'foobar',
+          template: '{{ (foo | async) === false }}'
+                        ~~~~~~~~~~~~~~~
+        })
+        class Test {}`);
+      });
+
+      it('fixes un-strict equality', () => {
+        let source = `
         @Component({
           selector: 'foobar',
           template: '{{ (foo | async) == false }}'
                          ~~~~~~~~~~~~~~~~~~~~~~
         })
-        class Test {
-          constructor(public foo: Observable<Boolean>) {}
-        }`;
-        assertAnnotated({
+        class Test {}`;
+        const failures =  assertAnnotated({
           ruleName: 'templates-no-negated-async',
           message: 'Async pipes must use strict equality `===` when comparing with `false`',
           source
         });
+
+        const res = Replacement.applyAll(source, failures[0].getFix());
+        expect(res).to.eq(`
+        @Component({
+          selector: 'foobar',
+          template: '{{ (foo | async) === false }}'
+                         ~~~~~~~~~~~~~~~~~~~~~~
+        })
+        class Test {}`);
+      });
     });
   });
 
