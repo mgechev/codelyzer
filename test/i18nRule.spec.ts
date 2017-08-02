c746bbc82a7b3890eef9f21fccdd4511ea8409d4
mgechev <mgechev@gmail.com>
2017-08-02 16:32:39 +0300

    feat: add i18n id check

---
+++ test/i18nRule.spec.ts
@@ -0,0 +1,81 @@
+import {
+  assertSuccess,
+  assertAnnotated,
+  assertMultipleAnnotated
+} from './testHelper';
+import { Replacement } from 'tslint';
+import { expect } from 'chai';
+import { FsFileResolver } from '../src/angular/fileResolver/fsFileResolver';
+import { MetadataReader } from '../src/angular/metadataReader';
+import * as ts from 'typescript';
+import chai = require('chai');
+
+const getAst = (code: string, file = 'file.ts') => {
+  return ts.createSourceFile(file, code, ts.ScriptTarget.ES5, true);
+};
+
+describe.only('i18n', () => {
+  describe('check-id', () => {
+    it('should work with proper id', () => {
+      let source = `
+      @Component({
+	template: \`
+	  <div i18n="test@@foo">Text</div>
+	\`
+      })
+      class Bar {}
+      `;
+      assertSuccess('i18n', source, ['check-id']);
+    });
+
+    it('should work with proper id', () => {
+      let source = `
+      @Component({
+	template: \`
+	  <div i18n="@@foo">Text</div>
+	\`
+      })
+      class Bar {}
+      `;
+      assertSuccess('i18n', source, ['check-id']);
+    });
+
+    it('should fail with missing id string', () => {
+      let source = `
+      @Component({
+	template: \`
+	  <div i18n="foo@@">Text</div>
+	       ~~~~~~~~~~~~
+	\`
+      })
+      class Bar {}
+      `;
+      assertAnnotated({
+	ruleName: 'i18n',
+	options: ['check-id'],
+	source,
+	message:
+	  'Missing custom message identifier. For more information visit https://angular.io/guide/i18n'
+      });
+    });
+
+    it('should fail with missing id', () => {
+      let source = `
+      @Component({
+	template: \`
+	  <div i18n="foo">Text</div>
+	       ~~~~~~~~~~
+	\`
+      })
+      class Bar {}
+      `;
+      assertAnnotated({
+	ruleName: 'i18n',
+	options: ['check-id'],
+	source,
+	message:
+	  'Missing custom message identifier. For more information visit https://angular.io/guide/i18n'
+      });
+    });
+  });
+});
