1e5de7481174efdb798f85ae0e3fc72d542ef8a6
reduckted <reduckted@outlook.com>
2017-10-24 04:32:10 +1000

    fix: add i18n rule to npm package (#435)
    Add the i18n rule to index.ts so that it is included in the npm package.
    
    Fix #433

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {Rule as ContextualLifeCycleRule} from './contextualLifeCycleRule';
 export {Rule as DecoratorNotAllowedRule} from './decoratorNotAllowedRule';
 export {Rule as DirectiveClassSuffixRule} from './directiveClassSuffixRule';
 export {Rule as DirectiveSelectorRule} from './directiveSelectorRule';
+export {Rule as I18nRule} from './i18nRule';
 export {Rule as ImportDestructuringSpacingRule} from './importDestructuringSpacingRule';
 export {Rule as InvokeInjectableRule} from './invokeInjectableRule';
 export {Rule as NoAccessMissingMemberRule} from './noAccessMissingMemberRule';
