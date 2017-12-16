58b72c317bb99dad619f7d805b8aaaaa54ee2ad7
Guillaume Bilodeau <guillaume.bilodeau@gmail.com>
2017-12-15 22:03:50 -0500

    feat(no-output-named-after-standard-event-rule): Initial version

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ export { Rule as ImportDestructuringSpacingRule } from './importDestructuringSpa
 export { Rule as NoAttributeParameterDecoratorRule } from './noAttributeParameterDecoratorRule';
 export { Rule as NoForwardRefRule } from './noForwardRefRule';
 export { Rule as NoInputRenameRule } from './noInputRenameRule';
+export { Rule as NoOutputNamedAfterStandardEventRule } from './noOutputNamedAfterStandardEventRule';
 export { Rule as NoOutputOnPrefixRule } from './noOutputOnPrefixRule';
 export { Rule as NoOutputRenameRule } from './noOutputRenameRule';
 export { Rule as NoUnusedCssRule } from './noUnusedCssRule';
---
+++ src/noOutputNamedAfterStandardEventRule.ts
@@ -0,0 +1,67 @@
+import * as Lint from 'tslint';
+import * as ts from 'typescript';
+import { sprintf } from 'sprintf-js';
+import { NgWalker } from './angular/ngWalker';
+
+export class Rule extends Lint.Rules.AbstractRule {
+  public static metadata: Lint.IRuleMetadata = {
+    ruleName: 'no-output-named-after-standard-event',
+    type: 'maintainability',
+    description: `Disallows naming directive outputs after a standard DOM event.`,
+    descriptionDetails: `???`,
+    rationale: `???`,
+    options: null,
+    optionsDescription: `Not configurable.`,
+    typescriptOnly: true,
+  };
+
+  static FAILURE_STRING: string = 'In the class "%s", the output ' +
+  'property "%s" should not be named after a standard event.';
+
+  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    return this.applyWithWalker(
+      new OutputMetadataWalker(sourceFile,
+        this.getOptions()));
+  }
+}
+
+
+export class OutputMetadataWalker extends NgWalker {
+  private readonly standardEventNames = [
+    'abort', 'afterprint', 'animationend', 'animationiteration', 'animationstart', 'appinstalled', 'audioprocess', 'audioend', 'audiostart',
+    'beforeprint', 'beforeunload', 'beginEvent', 'blocked', 'blur', 'boundary', 'cached', 'canplay', 'canplaythrough', 'change',
+    'chargingchange', 'chargingtimechange', 'checking', 'click', 'close', 'complete', 'compositionend', 'compositionstart',
+    'compositionupdate', 'contextmenu', 'copy', 'cut', 'dblclick', 'devicechange', 'devicelight', 'devicemotion', 'deviceorientation',
+    'deviceproximity', 'dischargingtimechange', 'DOMAttributeNameChanged', 'DOMAttrModified', 'DOMCharacterDataModified',
+    'DOMContentLoaded', 'DOMElementNameChanged', 'focus', 'focusin', 'focusout', 'DOMNodeInserted', 'DOMNodeInsertedIntoDocument',
+    'DOMNodeRemoved', 'DOMNodeRemovedFromDocument', 'DOMSubtreeModified', 'downloading', 'drag', 'dragend', 'dragenter', 'dragleave',
+    'dragover', 'dragstart', 'drop', 'durationchange', 'emptied', 'end', 'ended', 'endEvent', 'error', 'fullscreenchange',
+    'fullscreenerror', 'gamepadconnected', 'gamepaddisconnected', 'gotpointercapture', 'hashchange', 'lostpointercapture', 'input',
+    'invalid', 'keydown', 'keypress', 'keyup', 'languagechange', 'levelchange', 'load', 'loadeddata', 'loadedmetadata', 'loadend',
+    'loadstart', 'mark', 'message', 'messageerror', 'mousedown', 'mouseenter', 'mouseleave', 'mousemove', 'mouseout', 'mouseover',
+    'mouseup', 'nomatch', 'notificationclick', 'noupdate', 'obsolete', 'offline', 'online', 'open', 'orientationchange', 'pagehide',
+    'pageshow', 'paste', 'pause', 'pointercancel', 'pointerdown', 'pointerenter', 'pointerleave', 'pointerlockchange', 'pointerlockerror',
+    'pointermove', 'pointerout', 'pointerover', 'pointerup', 'play', 'playing', 'popstate', 'progress', 'push', 'pushsubscriptionchange',
+    'ratechange', 'readystatechange', 'repeatEvent', 'reset', 'resize', 'resourcetimingbufferfull', 'result', 'resume', 'scroll', 'seeked',
+    'seeking', 'select', 'selectstart', 'selectionchange', 'show', 'soundend', 'soundstart', 'speechend', 'speechstart', 'stalled',
+    'start', 'storage', 'submit', 'success', 'suspend', 'SVGAbort', 'SVGError', 'SVGLoad', 'SVGResize', 'SVGScroll', 'SVGUnload',
+    'SVGZoom', 'timeout', 'timeupdate', 'touchcancel', 'touchend', 'touchmove', 'touchstart', 'transitionend', 'unload', 'updateready',
+    'upgradeneeded', 'userproximity', 'voiceschanged', 'versionchange', 'visibilitychange', 'volumechange', 'waiting', 'wheel'
+  ];
+  visitNgOutput(property: ts.PropertyDeclaration, output: ts.Decorator, args: string[]) {
+    let className = (<any>property).parent.name.text;
+    let memberName = (<any>property.name).text;
+
+    if (memberName && this.standardEventNames.some(n => n === memberName)) {
+      const failureConfig: string[] = [Rule.FAILURE_STRING, className, memberName];
+      const errorMessage = sprintf.apply(null, failureConfig);
+      this.addFailure(
+        this.createFailure(
+          property.getStart(),
+          property.getWidth(),
+          errorMessage
+        )
+      );
+    }
+  }
+}
