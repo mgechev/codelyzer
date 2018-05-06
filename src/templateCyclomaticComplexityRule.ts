a4c22e93f04ec197f92e6c6cf71836c4df19766f
Rafael <rafaelsantana3095@gmail.com>
2018-05-06 12:07:24 -0300

    fix(rule): template-cyclomatic-complexity not reporting failures for '[ngForOf]' and '[ngIf]'

--- src/templateCyclomaticComplexityRule.ts
+++ src/templateCyclomaticComplexityRule.ts
@@ -1,35 +1,34 @@
-import * as Lint from 'tslint';
-import * as ts from 'typescript';
-import * as ast from '@angular/compiler';
+import { BoundDirectivePropertyAst } from '@angular/compiler';
 import { sprintf } from 'sprintf-js';
-import { BasicTemplateAstVisitor } from './angular/templates/basicTemplateAstVisitor';
+import { IRuleMetadata, RuleFailure, Rules } from 'tslint/lib';
+import { SourceFile } from 'typescript/lib/typescript';
 import { NgWalker } from './angular/ngWalker';
+import { BasicTemplateAstVisitor } from './angular/templates/basicTemplateAstVisitor';
 
-export class Rule extends Lint.Rules.AbstractRule {
-  public static metadata: Lint.IRuleMetadata = {
-    ruleName: 'template-cyclomatic-complexity',
-    type: 'functionality',
+export class Rule extends Rules.AbstractRule {
+  static readonly metadata: IRuleMetadata = {
     description:
       "Checks cyclomatic complexity against a specified limit. It is a quantitative measure of the number of linearly independent paths through a program's source code",
-    rationale: 'Cyclomatic complexity over some threshold indicates that the logic should be moved outside the template.',
+    optionExamples: ['true', '[true, 6]'],
     options: {
-      type: 'array',
       items: {
         type: 'string'
       },
+      maxLength: 2,
       minLength: 0,
-      maxLength: 2
+      type: 'array'
     },
-    optionExamples: ['true', '[true, 6]'],
     optionsDescription: 'Determine the maximum number of the cyclomatic complexity.',
+    rationale: 'Cyclomatic complexity over some threshold indicates that the logic should be moved outside the template.',
+    ruleName: 'template-cyclomatic-complexity',
+    type: 'functionality',
     typescriptOnly: true
   };
 
-  static COMPLEXITY_FAILURE_STRING = "The cyclomatic complexity exceeded the defined limit (cost '%s'). Your template should be refactored.";
+  static readonly FAILURE_STRING = "The cyclomatic complexity exceeded the defined limit (cost '%s'). Your template should be refactored.";
+  static readonly DEFAULT_MAX_COMPLEXITY = 5;
 
-  static COMPLEXITY_MAX = 5;
-
-  public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+  apply(sourceFile: SourceFile): RuleFailure[] {
     return this.applyWithWalker(
       new NgWalker(sourceFile, this.getOptions(), {
         templateVisitorCtrl: TemplateConditionalComplexityVisitor
@@ -38,33 +37,38 @@ export class Rule extends Lint.Rules.AbstractRule {
   }
 }
 
+export const getFailureMessage = (maxComplexity = Rule.DEFAULT_MAX_COMPLEXITY): string => {
+  return sprintf(Rule.FAILURE_STRING, maxComplexity);
+};
+
 class TemplateConditionalComplexityVisitor extends BasicTemplateAstVisitor {
-  complexity = 0;
+  totalComplexity = 0;
 
-  visitDirectiveProperty(prop: ast.BoundDirectivePropertyAst, context: BasicTemplateAstVisitor): any {
-    if (prop.sourceSpan) {
-      const directive = (<any>prop.sourceSpan).toString();
-
-      if (
-        directive.startsWith('*ngFor') ||
-        directive.startsWith('*ngIf') ||
-        directive.startsWith('*ngSwitchCase') ||
-        directive.startsWith('*ngSwitchDefault')
-      ) {
-        this.complexity++;
-      }
-    }
-
-    const options = this.getOptions();
-    const complexityMax: number = options.length ? options[0] : Rule.COMPLEXITY_MAX;
-
-    if (this.complexity > complexityMax) {
-      const span = prop.sourceSpan;
-      let failureConfig: string[] = [String(complexityMax)];
-      failureConfig.unshift(Rule.COMPLEXITY_FAILURE_STRING);
-      this.addFailure(this.createFailure(span.start.offset, span.end.offset - span.start.offset, sprintf.apply(this, failureConfig)));
-    }
-
+  visitDirectiveProperty(prop: BoundDirectivePropertyAst, context: any): any {
+    this.validateDirective(prop);
     super.visitDirectiveProperty(prop, context);
+  }
+
+  private validateDirective(prop: BoundDirectivePropertyAst): void {
+    const pattern = /^ng(ForOf|If|Switch(Case|Default))$/;
+    const { templateName } = prop;
+
+    if (pattern.test(templateName)) {
+      this.totalComplexity++;
+    }
+
+    const maxComplexity: number = this.getOptions()[0] || Rule.DEFAULT_MAX_COMPLEXITY;
+
+    if (this.totalComplexity <= maxComplexity) {
+      return;
+    }
+
+    const {
+      sourceSpan: {
+        end: { offset: endOffset },
+        start: { offset: startOffset }
+      }
+    } = prop;
+    this.addFailureFromStartToEnd(startOffset, endOffset, getFailureMessage(maxComplexity));
   }
 }
