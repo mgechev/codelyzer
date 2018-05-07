fedd331707e7112b91757083b1b0f830ef7a995f
Rafael Santana <rafaelsantana3095@gmail.com>
2018-05-07 07:07:22 -0300

    fix(rule): template-cyclomatic-complexity not reporting failures for '[ngForOf]' and '[ngIf]' (#612)

--- test/templateCyclomaticComplexityRule.spec.ts
+++ test/templateCyclomaticComplexityRule.spec.ts
@@ -1,59 +1,20 @@
-import { assertSuccess, assertAnnotated } from './testHelper';
+import { getFailureMessage, Rule } from '../src/templateCyclomaticComplexityRule';
+import { assertAnnotated, assertSuccess } from './testHelper';
 
-describe('cyclomatic complexity', () => {
-  describe('success', () => {
-    it('should work with a lower level of complexity', () => {
-      let source = `
-        @Component({
-          template: \`
-            <div *ngIf="a === '1'">
-              <li *ngFor="let person of persons; trackBy: trackByFn">
-                {{ person.name }}
-                <div [ngSwitch]="person.emotion">
-                  <app-happy-hero    *ngSwitchCase="'happy'" [hero]="currentHero"></app-happy-hero>
-                  <app-sad-hero      *ngSwitchCase="'sad'"   [hero]="currentHero"></app-sad-hero>
-                  <app-unknown-hero  *ngSwitchDefault        [hero]="currentHero"></app-unknown-hero>
-                </div>
-              </li>
-            </div>
-          \`
-        })
-        class Bar {}
-      `;
-      assertSuccess('template-cyclomatic-complexity', source);
-    });
+const {
+  metadata: { ruleName }
+} = Rule;
 
-    it('should work with a higher level of complexity', () => {
-      let source = `
-        @Component({
-          template: \`
-            <div *ngIf="a === '1'">
-              <li *ngFor="let person of persons; trackBy: trackByFn">
-                <div *ngIf="a === '1'">{{ person.name }}</div>
-                <div [ngSwitch]="person.emotion">
-                  <app-happy-hero    *ngSwitchCase="'happy'"    [hero]="currentHero"></app-happy-hero>
-                  <app-sad-hero      *ngSwitchCase="'sad'"      [hero]="currentHero"></app-sad-hero>
-                  <app-confused-hero *ngSwitchCase="'confused'" [hero]="currentHero"></app-confused-hero>
-                  <app-unknown-hero  *ngSwitchDefault           [hero]="currentHero"></app-unknown-hero>
-                </div>
-              </li>
-            </div>
-          \`
-        })
-        class Bar {}
-      `;
-      assertSuccess('template-cyclomatic-complexity', source, [7]);
-    });
-  });
-
+describe(ruleName, () => {
   describe('failure', () => {
     it('should fail with a higher level of complexity', () => {
-      let source = `
+      const source = `
         @Component({
           template: \`
             <div *ngIf="a === '1'">
-              <li *ngFor="let person of persons; trackBy: trackByFn">
+              <div *ngFor="let person of persons; trackBy: trackByFn">
                 <div *ngIf="a === '1'">{{ person.name }}</div>
+
                 <div [ngSwitch]="person.emotion">
                   <app-happy-hero    *ngSwitchCase="'happy'"    [hero]="currentHero"></app-happy-hero>
                   <app-sad-hero      *ngSwitchCase="'sad'"      [hero]="currentHero"></app-sad-hero>
@@ -61,17 +22,102 @@ describe('cyclomatic complexity', () => {
                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~
                   <app-unknown-hero  *ngSwitchDefault           [hero]="currentHero"></app-unknown-hero>
                 </div>
-              </li>
+              </div>
             </div>
           \`
         })
         class Bar {}
       `;
       assertAnnotated({
-        ruleName: 'template-cyclomatic-complexity',
-        message: "The cyclomatic complexity exceeded the defined limit (cost '5'). Your template should be refactored.",
+        message: getFailureMessage(),
+        ruleName,
         source
       });
+    });
+
+    it('should fail with a higher level of complexity using directives with ng-template', () => {
+      const source = `
+        @Component({
+          template: \`
+            <div [fakeDirective]="'test'"></div>
+
+            <ng-template ngFor let-person [ngForOf]="persons" let-i="index">
+              {{ person.name }}
+            </ng-template>
+
+            <ng-template [ngIf]="a === '1'">
+              something here
+            </ng-template>
+
+            <div *ngIf="a === '1'">
+              <div *ngFor="let person of persons; trackBy: trackByFn">
+                <div *ngIf="a === '1'">{{ person.name }}</div>
+
+                <div [ngSwitch]="person.emotion">
+                  <app-happy-hero    *ngSwitchCase="'happy'"    [hero]="currentHero"></app-happy-hero>
+                  <app-sad-hero      *ngSwitchCase="'sad'"      [hero]="currentHero"></app-sad-hero>
+                                     ~~~~~~~~~~~~~~~~~~~~~
+                  <app-confused-hero *ngSwitchCase="'confused'" [hero]="currentHero"></app-confused-hero>
+                  <app-unknown-hero  *ngSwitchDefault           [hero]="currentHero"></app-unknown-hero>
+                </div>
+              </div>
+            </div>
+          \`
+        })
+        class Bar {}
+      `;
+      assertAnnotated({
+        message: getFailureMessage(6),
+        options: [6],
+        ruleName,
+        source
+      });
+    });
+  });
+
+  describe('success', () => {
+    it('should work with a lower level of complexity', () => {
+      const source = `
+        @Component({
+          template: \`
+            <div *ngIf="a === '1'">
+              <div *ngFor="let person of persons; trackBy: trackByFn">
+                {{ person.name }}
+                <div [ngSwitch]="person.emotion">
+                  <app-happy-hero    *ngSwitchCase="'happy'" [hero]="currentHero"></app-happy-hero>
+                  <app-sad-hero      *ngSwitchCase="'sad'"   [hero]="currentHero"></app-sad-hero>
+                  <app-unknown-hero  *ngSwitchDefault        [hero]="currentHero"></app-unknown-hero>
+                </div>
+              </div>
+            </div>
+          \`
+        })
+        class Bar {}
+      `;
+      assertSuccess(ruleName, source);
+    });
+
+    it('should work with a higher level of complexity', () => {
+      const source = `
+        @Component({
+          template: \`
+            <div *ngIf="a === '1'">
+              <div *ngFor="let person of persons; trackBy: trackByFn">
+                <div *ngIf="a === '1'">{{ person.name }}</div>
+
+                <div [ngSwitch]="person.emotion">
+                  <app-happy-hero    *ngSwitchCase="'happy'"    [hero]="currentHero"></app-happy-hero>
+                  <app-sad-hero      *ngSwitchCase="'sad'"      [hero]="currentHero"></app-sad-hero>
+                  <app-confused-hero *ngSwitchCase="'confused'" [hero]="currentHero"></app-confused-hero>
+                  <app-unknown-hero  *ngSwitchDefault           [hero]="currentHero"></app-unknown-hero>
+                </div>
+              </div>
+            </div>
+          \`
+        })
+        class Bar {}
+      `;
+      assertSuccess(ruleName, source, [7]);
     });
   });
 });
