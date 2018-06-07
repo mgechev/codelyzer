e5b16a800c1121fb620f8ad4cb58c8a32a22b559
Priyanka Shete <shete@mehrkanal.com>
2018-06-07 22:00:57 +0200

    feat: externalizing template, css visitor abstractions and ngwalker

---
+++ src/angular/index.ts
@@ -0,0 +1,7 @@
+export * from './templates/basicTemplateAstVisitor';
+
+export * from './styles/basicCssAstVisitor';
+
+export * from './config';
+export * from './metadata';
+export * from './ngWalker';
