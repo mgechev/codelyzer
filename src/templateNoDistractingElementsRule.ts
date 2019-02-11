54cb7b7d69192729372b383b5bd8516db8ec1c3a
Zama Khan Mohammed <zama.mohammed@tekzenit.com>
2019-02-11 16:57:44 -0600

    feat(rule): no distracting elements should be used

---
+++ src/templateNoDistractingElementsRule.ts
@@ -0,0 +1,51 @@
+import { ElementAst } from '@angular/compiler';
+import { sprintf } from 'sprintf-js';
+import { IRuleMetadata, RuleFailure, Rules } from 'tslint/lib';
+import { SourceFile } from 'typescript/lib/typescript';
+import { NgWalker } from './angular/ngWalker';
+import { BasicTemplateAstVisitor } from './angular/templates/basicTemplateAstVisitor';
+
+export class Rule extends Rules.AbstractRule {
+  static readonly metadata: IRuleMetadata = {
+    description: 'Enforces that no distracting elements are used',
+    options: null,
+    optionsDescription: 'Not configurable.',
+    rationale: 'Elements that can be visually distracting can cause accessibility issues with visually impaired users.',
+    ruleName: 'template-no-distracting-elements',
+    type: 'functionality',
+    typescriptOnly: true
+  };
+
+  static readonly FAILURE_STRING = 'Avoid using <%s/> elements as they create visual accessibility issues.';
+
+  apply(sourceFile: SourceFile): RuleFailure[] {
+    return this.applyWithWalker(
+      new NgWalker(sourceFile, this.getOptions(), {
+        templateVisitorCtrl: TemplateNoDistractingElementsVisitor
+      })
+    );
+  }
+}
+
+export function getFailureMessage(element: string) {
+  return sprintf(Rule.FAILURE_STRING, element);
+}
+
+class TemplateNoDistractingElementsVisitor extends BasicTemplateAstVisitor {
+  visitElement(prop: ElementAst, context: any): any {
+    this.validateElement(prop);
+    super.visitElement(prop, context);
+  }
+
+  private validateElement(el: ElementAst): void {
+    if (el.name === 'marquee' || el.name === 'blink') {
+      const {
+        sourceSpan: {
+          end: { offset: endOffset },
+          start: { offset: startOffset }
+        }
+      } = el;
+      this.addFailureFromStartToEnd(startOffset, endOffset, getFailureMessage(el.name));
+    }
+  }
+}
