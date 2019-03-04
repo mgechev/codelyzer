720bde8c4ddb83c10b64059edba2568859e9d364
Rafael Santana <rafaelsantana3095@gmail.com>
2019-03-04 16:32:04 -0300

    refactor: rework SelectorPropertyBase and its descendants

--- src/componentSelectorRule.ts
+++ src/componentSelectorRule.ts
@@ -1,28 +1,37 @@
-import { IRuleMetadata, Utils } from 'tslint/lib';
-import { SelectorRule } from './selectorNameBase';
+import { IRuleMetadata } from 'tslint/lib';
+import { arrayify, dedent } from 'tslint/lib/utils';
+import {
+  OPTION_STYLE_CAMEL_CASE,
+  OPTION_STYLE_KEBAB_CASE,
+  OPTION_TYPE_ATTRIBUTE,
+  OPTION_TYPE_ELEMENT,
+  SelectorPropertyBase,
+  SelectorStyle,
+  SelectorType
+} from './selectorPropertyBase';
+import { isNotNullOrUndefined } from './util/is-not-null-or-undefined';
 
-const OPTION_ATTRIBUTE = 'attribute';
-const OPTION_ELEMENT = 'element';
-const OPTION_CAMEL_CASE = 'camelCase';
-const OPTION_KEBAB_CASE = 'kebab-case';
+const STYLE_GUIDE_PREFIX_LINK = 'https://angular.io/guide/styleguide#style-02-07';
+const STYLE_GUIDE_STYLE_LINK = 'https://angular.io/guide/styleguide#style-05-02';
+const STYLE_GUIDE_TYPE_LINK = 'https://angular.io/guide/styleguide#style-05-03';
 
-export class Rule extends SelectorRule {
+export class Rule extends SelectorPropertyBase {
   static readonly metadata: IRuleMetadata = {
     description: 'Component selectors should follow given naming rules.',
-    descriptionDetails: Utils.dedent`
-      See more at https://angular.io/styleguide#style-02-07, https://angular.io/styleguide#style-05-02,
-      and https://angular.io/styleguide#style-05-03.
+    descriptionDetails: dedent`
+      See more at ${STYLE_GUIDE_PREFIX_LINK}, ${STYLE_GUIDE_STYLE_LINK}
+      and ${STYLE_GUIDE_TYPE_LINK}.
     `,
     optionExamples: [
-      [true, OPTION_ELEMENT, 'my-prefix', OPTION_KEBAB_CASE],
-      [true, OPTION_ELEMENT, ['ng', 'ngx'], OPTION_KEBAB_CASE],
-      [true, OPTION_ATTRIBUTE, 'myPrefix', OPTION_CAMEL_CASE],
-      [true, [OPTION_ELEMENT, OPTION_ATTRIBUTE], 'myPrefix', OPTION_CAMEL_CASE]
+      [true, OPTION_TYPE_ELEMENT, 'my-prefix', OPTION_STYLE_KEBAB_CASE],
+      [true, OPTION_TYPE_ELEMENT, ['ng', 'ngx'], OPTION_STYLE_KEBAB_CASE],
+      [true, OPTION_TYPE_ATTRIBUTE, 'myPrefix', OPTION_STYLE_CAMEL_CASE],
+      [true, [OPTION_TYPE_ELEMENT, OPTION_TYPE_ATTRIBUTE], 'myPrefix', OPTION_STYLE_CAMEL_CASE]
     ],
     options: {
       items: [
         {
-          enum: [OPTION_ATTRIBUTE, OPTION_ELEMENT]
+          enum: [OPTION_TYPE_ATTRIBUTE, OPTION_TYPE_ELEMENT]
         },
         {
           oneOf: [
@@ -38,20 +47,20 @@ export class Rule extends SelectorRule {
           ]
         },
         {
-          enum: [OPTION_CAMEL_CASE, OPTION_KEBAB_CASE]
+          enum: [OPTION_STYLE_CAMEL_CASE, OPTION_STYLE_KEBAB_CASE]
         }
       ],
       maxLength: 3,
       minLength: 3,
       type: 'array'
     },
-    optionsDescription: Utils.dedent`
+    optionsDescription: dedent`
       Options accept three obligatory items as an array:
-      1. \`${OPTION_ELEMENT}\` or \`${OPTION_ATTRIBUTE}\` forces components to be used as either elements, attributes, or both (not recommended)
+      1. \`${OPTION_TYPE_ELEMENT}\` or \`${OPTION_TYPE_ATTRIBUTE}\` forces components to be used as either elements, attributes, or both (not recommended)
       2. A single prefix (string) or array of prefixes (strings) which have to be used in component selectors.
-      3. \`${OPTION_KEBAB_CASE}\` or \`${OPTION_CAMEL_CASE}\` allows you to pick a case.
+      3. \`${OPTION_STYLE_KEBAB_CASE}\` or \`${OPTION_STYLE_CAMEL_CASE}\` allows you to pick a case.
     `,
-    rationale: Utils.dedent`
+    rationale: dedent`
       * Consistent conventions make it easy to quickly identify and reference assets of different types.
       * Makes it easier to promote and share the component in other apps.
       * Components are easy to identify in the DOM.
@@ -67,30 +76,44 @@ export class Rule extends SelectorRule {
 
   handleType = 'Component';
 
-  getPrefixFailure(prefixes: string[]): string {
-    if (prefixes.length === 1) {
-      return 'The selector of the component "%s" should have prefix "%s" (https://angular.io/styleguide#style-02-07)';
-    } else {
-      return 'The selector of the component "%s" should have one of the prefixes "%s" (https://angular.io/styleguide#style-02-07)';
-    }
+  getPrefixFailure(prefixes: ReadonlyArray<string>): string {
+    const prefixStr = prefixes.length === 1 ? '' : ' one of the prefixes:';
+
+    return `The selector should be prefixed by${prefixStr} "${prefixes.join(', ')}" (${STYLE_GUIDE_PREFIX_LINK})`;
   }
 
-  getStyleFailure(): string {
-    return 'The selector of the component "%s" should be named %s (https://angular.io/styleguide#style-05-02)';
+  getStyleFailure(style: SelectorStyle): string {
+    const styleStr = style === OPTION_STYLE_KEBAB_CASE ? `${OPTION_STYLE_KEBAB_CASE}d and include a dash` : `${OPTION_STYLE_CAMEL_CASE}d`;
+
+    return `The selector should be ${styleStr} (${STYLE_GUIDE_STYLE_LINK})`;
   }
 
-  getTypeFailure(): string {
-    return 'The selector of the component "%s" should be used as %s (https://angular.io/styleguide#style-05-03)';
+  getTypeFailure(types: ReadonlyArray<SelectorType>): string {
+    return `The selector should be used as an ${types.join(' or ')} (${STYLE_GUIDE_TYPE_LINK})`;
   }
 
   isEnabled(): boolean {
     const {
       metadata: {
-        options: { maxLength, minLength }
+        options: {
+          items: {
+            [0]: { enum: enumTypes },
+            [2]: { enum: enumStyles }
+          },
+          maxLength,
+          minLength
+        }
       }
     } = Rule;
-    const { length } = this.ruleArguments;
+    const { length: argumentsLength } = this.ruleArguments;
+    const typeArgument = arrayify<SelectorType>(this.ruleArguments[0]);
+    const prefixArgument = arrayify<string>(this.ruleArguments[1]).filter(isNotNullOrUndefined);
+    const styleArgument = this.ruleArguments[2] as SelectorStyle;
+    const argumentsLengthInRange = argumentsLength >= minLength && argumentsLength <= maxLength;
+    const isTypeArgumentValid = typeArgument.length > 0 && typeArgument.every(argument => enumTypes.indexOf(argument) !== -1);
+    const isPrefixArgumentValid = prefixArgument.length > 0;
+    const isStyleArgumentValid = enumStyles.indexOf(styleArgument) !== -1;
 
-    return super.isEnabled() && length >= minLength && length <= maxLength;
+    return super.isEnabled() && argumentsLengthInRange && isTypeArgumentValid && isPrefixArgumentValid && isStyleArgumentValid;
   }
 }
---
+++ src/selectorPropertyBase.ts
@@ -0,0 +1,133 @@
+import { CssSelector } from '@angular/compiler';
+import { IOptions, RuleFailure, WalkContext } from 'tslint';
+import { AbstractRule } from 'tslint/lib/rules';
+import { arrayify } from 'tslint/lib/utils';
+import {
+  ClassDeclaration,
+  createNodeArray,
+  Decorator,
+  forEachChild,
+  isClassDeclaration,
+  Node,
+  SourceFile,
+  StringLiteralLike
+} from 'typescript';
+import { SelectorValidator } from './util/selectorValidator';
+import { getDecoratorName, getDecoratorPropertyInitializer, isStringLiteralLike } from './util/utils';
+
+export const OPTION_STYLE_CAMEL_CASE = 'camelCase';
+export const OPTION_STYLE_KEBAB_CASE = 'kebab-case';
+export const OPTION_TYPE_ATTRIBUTE = 'attribute';
+export const OPTION_TYPE_ATTRS = 'attrs';
+export const OPTION_TYPE_ELEMENT = 'element';
+
+export type SelectorStyle = typeof OPTION_STYLE_CAMEL_CASE | typeof OPTION_STYLE_KEBAB_CASE;
+export type SelectorType = typeof OPTION_TYPE_ATTRIBUTE | typeof OPTION_TYPE_ELEMENT;
+export type SelectorTypeInternal = typeof OPTION_TYPE_ATTRS | typeof OPTION_TYPE_ELEMENT;
+
+const SELECTOR_TYPE_MAPPER: Record<SelectorType, SelectorTypeInternal> = {
+  [OPTION_TYPE_ATTRIBUTE]: OPTION_TYPE_ATTRS,
+  [OPTION_TYPE_ELEMENT]: OPTION_TYPE_ELEMENT
+};
+
+export abstract class SelectorPropertyBase extends AbstractRule {
+  abstract readonly handleType: string;
+  internalTypes: ReadonlyArray<SelectorType>;
+  prefixes: ReadonlyArray<string>;
+  style: SelectorStyle;
+  types: ReadonlyArray<SelectorTypeInternal>;
+
+  constructor(options: IOptions) {
+    super(options);
+
+    const { ruleArguments } = this.getOptions();
+
+    this.internalTypes = arrayify(ruleArguments[0]);
+    this.prefixes = arrayify<SelectorStyle>(ruleArguments[1]);
+    this.style = ruleArguments[2];
+    this.types = arrayify<SelectorType>(ruleArguments[0] || [OPTION_TYPE_ATTRIBUTE, OPTION_TYPE_ELEMENT]).reduce<
+      ReadonlyArray<SelectorTypeInternal>
+    >((previousValue, currentValue) => previousValue.concat(SELECTOR_TYPE_MAPPER[currentValue]), []);
+  }
+
+  abstract getPrefixFailure(prefixes: ReadonlyArray<string>): string;
+  abstract getStyleFailure(style: SelectorStyle): string;
+  abstract getTypeFailure(types: ReadonlyArray<SelectorType>): string;
+
+  getValidSelectors(selectors: CssSelector[]): ReadonlyArray<string> {
+    return selectors.reduce<ReadonlyArray<string>>((previousValue, currentValue) => {
+      const validSelectors = this.types
+        .reduce<ReadonlyArray<string>>((accumulator, type) => {
+          const value = currentValue[type];
+
+          return value ? accumulator.concat(value) : accumulator;
+        }, [])
+        .filter(Boolean);
+
+      return previousValue.concat(validSelectors);
+    }, []);
+  }
+
+  validatePrefixes(selectors: ReadonlyArray<string>): boolean {
+    return selectors.some(selector => this.prefixes.some(prefix => SelectorValidator.prefix(prefix, this.style)(selector)));
+  }
+
+  validateStyle(selectors: ReadonlyArray<string>): boolean {
+    const validator = this.style === OPTION_STYLE_KEBAB_CASE ? SelectorValidator.kebabCase : SelectorValidator.camelCase;
+
+    return selectors.some(selector => validator(selector));
+  }
+
+  validateTypes(selectors: ReadonlyArray<string>): boolean {
+    return selectors.length > 0;
+  }
+
+  apply(sourceFile: SourceFile): RuleFailure[] {
+    return this.applyWithFunction(sourceFile, walk, this);
+  }
+}
+
+const validateClassDeclaration = (context: WalkContext<SelectorPropertyBase>, node: ClassDeclaration): void =>
+  createNodeArray(node.decorators).forEach(decorator => validateDecorator(context, decorator));
+
+const validateDecorator = (context: WalkContext<SelectorPropertyBase>, decorator: Decorator): void => {
+  const selectorExpression = getDecoratorPropertyInitializer(decorator, 'selector');
+  const decoratorName = getDecoratorName(decorator);
+
+  if (!selectorExpression || !isStringLiteralLike(selectorExpression) || context.options.handleType !== decoratorName) {
+    return;
+  }
+
+  validateSelector(context, selectorExpression);
+};
+
+const validateSelector = (context: WalkContext<SelectorPropertyBase>, expression: StringLiteralLike): void => {
+  const { options } = context;
+  const selectors = CssSelector.parse(expression.text);
+  const validSelectors = options.getValidSelectors(selectors);
+  let failure: string | undefined;
+
+  if (!options.validateTypes(validSelectors)) {
+    failure = options.getTypeFailure(options.internalTypes);
+  } else if (!options.validateStyle(validSelectors)) {
+    failure = options.getStyleFailure(options.style);
+  } else if (!options.validatePrefixes(validSelectors)) {
+    failure = options.getPrefixFailure(options.prefixes);
+  }
+
+  if (failure) {
+    context.addFailureAtNode(expression, failure);
+  }
+};
+
+const walk = (context: WalkContext<SelectorPropertyBase>): void => {
+  const { sourceFile } = context;
+
+  const callback = (node: Node): void => {
+    if (isClassDeclaration(node)) validateClassDeclaration(context, node);
+
+    forEachChild(node, callback);
+  };
+
+  forEachChild(sourceFile, callback);
+};
